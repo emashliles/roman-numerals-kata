d3c4b745ecf8bb7ed998275a2ffc9a92a09701b1
EA <emashliles@gmail.com>
2017-01-17 11:12:20 +0000

    Proposed soloution.

--- lib/roman_numerals_kata.ex
+++ lib/roman_numerals_kata.ex
@@ -1,2 +1,21 @@
 defmodule RomanNumeralsKata do
+
+  def convert(0), do: ""
+  def convert(arabic), do: convert(arabic, "")
+
+  def convert(arabic, roman) do
+    cond do
+      arabic >= 10 -> 
+        convert(arabic - 10, roman <> "X")
+      arabic == 9 ->
+        convert(arabic- 9, roman <> "IX")
+      arabic >= 5 -> 
+        convert(arabic - 5, roman <> "V")
+      arabic == 4 ->
+          convert(arabic - 4, roman <> "IV")
+      true ->
+        ones = List.duplicate("I", arabic) |> Enum.join
+        roman <> ones
+    end
+  end
 end
--- test/roman_numerals_kata_test.exs
+++ test/roman_numerals_kata_test.exs
@@ -2,7 +2,43 @@ defmodule RomanNumeralsKataTest do
   use ExUnit.Case
   doctest RomanNumeralsKata
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "converts 0" do
+    assert RomanNumeralsKata.convert(0) == ""
+  end
+
+  test "converts 1" do
+    assert RomanNumeralsKata.convert(1) == "I"
+  end
+
+  test "converts 2" do
+    assert RomanNumeralsKata.convert(2) == "II"
+  end
+
+  test "converts 3" do
+    assert RomanNumeralsKata.convert(3) == "III"
+  end
+
+  test "converts 4" do
+    assert RomanNumeralsKata.convert(4) == "IV"
+  end
+
+  test "converts 5" do
+    assert RomanNumeralsKata.convert(5) == "V"
+  end
+
+  test "converts 6" do
+    assert RomanNumeralsKata.convert(6) == "VI"
+  end
+
+  test "converts 9" do
+    assert RomanNumeralsKata.convert(9) == "IX"
+  end
+
+  test "converts 10" do
+    assert RomanNumeralsKata.convert(10) == "X"
+  end
+
+  test "converts 14" do
+    assert RomanNumeralsKata.convert(14) == "XIV"
   end
 end
